f17ce243031ebed0f9da73dfa5b4b08bdb521c62
Amir-Gaifullin <47780452+Amir-Gaifullin@users.noreply.github.com>
2021-06-28 00:36:58 +0300

    Add files via upload

---
+++ autor_register/js/app.js
@@ -0,0 +1,16 @@
+
+let header = document.querySelector("#header");
+let main_content = document.querySelector("#main-content");
+let main_content_height = main_content.clientHeight;
+let scroll_position = window.scrollY;
+
+document.addEventListener("scroll", function() {
+    scroll_position = window.scrollY;
+
+    if(scroll_position > 88.88){
+        header.classList.add("fixed");
+    }
+    else {
+        header.classList.remove("fixed")
+    }
+}, false);
